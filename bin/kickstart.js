6099de386de5b32732a4d235266622596339b457
Youssef Mansour <contact@youssef.fi>
2020-04-28 16:32:46 +0300

    Added: react-app|ra command to kickstart

--- bin/kickstart.js
+++ bin/kickstart.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
-require = require("esm")(module /*, options*/);
-require("../src/cli").cli(process.argv);
+const program = require("commander");
+const pkg = require("../package.json");
+const kickstart_commands = require("../commands/kickstart");
+
+const { react_app } = kickstart_commands;
+
+program
+  .version(pkg.version)
+  .command("react-app")
+  .description("Create react app.")
+  .alias("ra")
+  .option("-t, --typescript", "Create typescript template.", false)
+  .option("-n, --project-name <type>", "flavour of pizza")
+  .action((options) => {
+    react_app(options);
+  });
+program.parse(process.argv);
