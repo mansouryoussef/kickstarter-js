df3ce7969ef78161c455a0a5256d0236c5035239
Youssef Mansour <contact@youssef.fi>
2020-05-03 17:13:39 +0300

    Modified: react-component|rc command options

--- bin/kickstart-react-component.js
+++ bin/kickstart-react-component.js
@@ -1,13 +1,18 @@
 const program = require("commander");
-// const action = require("../commands/react-component");
+const action = require("../commands/react-component");
 
 program
   .option("-n, --component-name <string>", "Component name.")
   .option("-c,--class-based ", "Create class-based component.", false)
+  .option("-ts,--typescript ", "Create typescript component.", false)
   .option("--scss", "Create component with scss stylesheet.", false)
-  .option("--scss-module", "Create component with scss module.", false)
-  .option("--css-module", "Create component with css module.", false)
+  .option(
+    "-m,--stylesheet-module",
+    "Create component with stylesheet module.",
+    false
+  )
+
   .action((options) => {
-    // action(options);
+    action(options);
   });
 program.parse(process.argv);
